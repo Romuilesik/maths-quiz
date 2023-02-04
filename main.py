c83375802849e550f7b95113772b92a3586b4482
Romuilesik <roma.ilesik@gmail.com>
2023-02-04 13:08:48 +0200

    maths commit

---
+++ main.py
@@ -0,0 +1,81 @@
+import time
+answer = 0
+
+
+print("Question #1")
+print()
+print("14*21")
+
+user = input ("Input a number: ")
+
+
+if user == "294":
+    answer += 1
+
+
+time.sleep(0.5)
+
+print()
+print("Question #2")
+print("√9")
+user = input ("Input a number: ")
+
+if user == "3":
+    answer += 1
+
+
+time.sleep(0.5)
+
+print()
+print("Question #3")
+print("168+784-4*3")
+user = input ("Input a number: ")
+
+if user == "940":
+    answer += 1
+
+time.sleep(0.5)
+
+print()
+print("Question #4")
+print("99*99")
+user = input ("Input a number: ")
+
+if user == "9801":
+    answer += 1
+
+time.sleep(0.5)
+
+print()
+print("Question #5")
+print("4*87:3")
+user = input ("Input a number: ")
+
+if user == "116":
+    answer += 1
+
+time.sleep(0.5)
+
+print()
+print("LAST QUESTION")
+print("478+474-253:2-0.5")
+user = input ("Input a number: ")
+
+if user == "825":
+    answer += 1
+
+
+print()
+print("Your results")
+time.sleep(0.5)
+print("papapapaaaaam")
+time.sleep(0.5)
+print(answer)
+
+
+
+
+
+
+
+
